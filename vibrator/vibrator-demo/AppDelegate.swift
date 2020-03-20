d9cb692e4101f6133e99e6e54a3b86b50be4943e
Daniel Storm <danielstormapps@gmail.com>
2020-03-20 16:54:41 -0400

    Add `prepare()` to reduce lag on initial haptic playback.

--- vibrator/vibrator-demo/AppDelegate.swift
+++ vibrator/vibrator-demo/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: - UIApplicationDelegate
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Vibrator.shared.prepare()
         return true
     }
 
